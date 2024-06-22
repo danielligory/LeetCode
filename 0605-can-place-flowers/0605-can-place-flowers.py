bfbdc5dc95ec7793ff30c1e74d91cc922a14a8f6
Daniel Ligory <daniel.ligory@gmail.com>
2024-06-22 22:41:33 +0100

    Time: 130 ms (65.05%), Space: 17 MB (24.69%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(length):
             if flowerbed[i] == 0:
                 prev_empty = (i == 0 or flowerbed[i-1] == 0)
-                print (f"{i} is {prev_empty} for prev_empty and {i == 0} and {flowerbed[i-1] == 0}")
+                #print (f"{i} is {prev_empty} for prev_empty and {i == 0} and {flowerbed[i-1] == 0}")
                 next_empty = (i == length - 1or flowerbed[i+1] == 0)
-                print (f"{i} is {prev_empty} for next_empty and {i == 0} and {flowerbed[i-1] == 0}\n")
+                #print (f"{i} is {prev_empty} for next_empty and {i == 0} and {flowerbed[i-1] == 0}\n")
                 
                 if prev_empty and next_empty:
                     flowerbed[i] = 1
