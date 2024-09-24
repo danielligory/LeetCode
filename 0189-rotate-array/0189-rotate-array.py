fc37215d42685cd25013fa3dfc02492a1bd5c4b4
Daniel Ligory <daniel.ligory@gmail.com>
2024-09-24 13:03:02 +0100

    Time: 140 ms (98.59%), Space: 28 MB (29.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,9 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        x = 0
-        while (k != 0):
-            x = nums[-1]
-            nums.insert(0, x)
-            nums.pop(-1)
-            k = k - 1
+        n = len(nums)
+        k = k % n
+        
+        nums2 = nums.copy()
+        
+        for i in range(n):
+            nums[(i + k) % n] = nums2[i]
